484bdd1c88b104baad71ac733074cf3b1cdfb506
bwnyasse <risbonyasse@gmail.com>
2016-08-12 00:12:12 +0200

    Code refactoring, move fluentd-led to led

---
+++ images/led/www/js/env.js
@@ -0,0 +1,5 @@
+env = {
+    ES_BROWSER_HOST: 'localhost',
+    ES_PORT: '9200',
+    ES_INDEX: 'fluentd'
+}
--- make.sh
+++ make.sh
@@ -1,16 +1,23 @@
+CURRENT=$(pwd)
+
 echo 'Build UI Dart2JS'
-cd ui && pub build --mode=release && cd ..
+cd $CURRENT/ui && pub get && pub build --mode=release
 
 ## Clean Previous ui
 echo 'Clean Previous www content'
-rm -rf images/fluentd-led/www/*
+rm -rf $CURRENT/images/led/www
 
 ## Copy Ui output to docker image dir
 echo 'Update www content'
-mv ui/build/web/* images/fluentd-led/www/
+mkdir $CURRENT/images/led/www
+mv $CURRENT/ui/build/web/* $CURRENT/images/led/www/
 
 ## Build fluentd LED docker image
 ## TODO
 
 ## Dev mode launch compose-explorer
-cd compose-explorer && docker-compose build  && docker-compose up --force-recreate
+
+cd $CURRENT/compose-explorer && docker-compose build && docker-compose up --force-recreate
+
+# Clean
+rm -rf $CURRENT/images/fluentd-led/www/
