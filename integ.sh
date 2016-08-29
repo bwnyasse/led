43bf2e18abda25bbde0c16a569fb64ad698cdea0
bwnyasse <risbonyasse@gmail.com>
2016-08-29 22:25:56 +0200

    Clean script to launch integration

--- integ.sh
+++ integ.sh
@@ -6,7 +6,51 @@
 # @author bwnyasse
 ##
 
+FLAG_M=false
+FLAG_S=false
+
+usage() {
+	cat <<-EOF
+
+  Script used to launch integration
+
+	OPTIONS:
+	========
+  -s, --skip-build      skip build for required project
+  -m, --maven-build     launch maven build for required project
+  -h                    show this help
+
+	EOF
+}
+
+#################
+### Read options
+################
+while true; do
+    case "$1" in
+        -m | --maven-build)
+          FLAG_M=true
+          break ;;
+        -s | --skip-build)
+          FLAG_S=true
+          break ;;
+        *|-h)
+            usage
+            exit 1 ;;
+    esac
+done
+
+#############################
+### Effectif Script build ###
+############################
+launchOption=''
+if $FLAG_M; then
+    launchOption='-m'
+elif $FLAG_S; then
+    launchOption='-s'
+fi
+
 CURRENT=$(pwd)
 INTEGRATION_PATH=$CURRENT/integ
 
-cd $INTEGRATION_PATH && ./launch.sh
+cd $INTEGRATION_PATH && ./launch.sh $launchOption
--- integ/launch.sh
+++ integ/launch.sh
@@ -6,6 +6,48 @@
 # @author bwnyasse
 ##
 
-mvn clean install -f ./javaee/wildfly-swarm/ && \
-docker-compose -f led.yml up --build --force-recreate -d && \
-docker-compose -f service.yml up --build --force-recreate -d
+FLAG_M=false
+FLAG_S=false
+
+usage() {
+	cat <<-EOF
+
+  Script used to launch integration
+
+	OPTIONS:
+	========
+  -s, --skip-build      skip build for required project
+  -m, --maven-build     launch maven build for required project
+  -h                    show this help
+
+	EOF
+}
+
+function launchCompose {
+  docker-compose -f led.yml up --build --force-recreate -d && \
+  docker-compose -f service.yml up --build --force-recreate -d
+}
+
+#################
+### Read options
+################
+while true; do
+    case "$1" in
+        -m | --maven-build)
+          FLAG_M=true
+          break ;;
+        -s | --skip-build)
+          FLAG_S=true
+          break ;;
+        *|-h)
+            usage
+            exit 1 ;;
+    esac
+done
+
+if $FLAG_M; then
+  mvn clean install -f ./javaee/wildfly-swarm/ && \
+  launchCompose
+elif $FLAG_S; then
+  launchCompose
+fi
