8bfadaf5ef8602f93c39bde8975ab018e77eb370
bwnyasse <risbonyasse@gmail.com>
2016-08-31 23:49:09 +0200

    Fix docker tag for deploying addendum

--- bin/dockerhub.sh
+++ bin/dockerhub.sh
@@ -8,5 +8,5 @@
 set -e
 
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-docker tag -f ${DOCKER_REPOSITORY}:${VERSION} ${DOCKER_REPOSITORY}:latest
+docker tag  ${DOCKER_REPOSITORY}:${VERSION} ${DOCKER_REPOSITORY}:latest
 docker push ${DOCKER_REPOSITORY}:${VERSION}
