2710b72ad8fae2c565034d20e6abd8580a0af257
bwnyasse <risbonyasse@gmail.com>
2016-09-01 21:47:14 +0200

    [TRAVIS] ensure fetch and pull from origin

--- bin/git_push_tag.sh
+++ bin/git_push_tag.sh
@@ -25,7 +25,8 @@ git config user.name "builds@travis-ci.com"
 git config user.email "Travis CI"
 
 git remote add origin "https://$GH_TOKEN@github.com/bwnyasse/fluent-led.git"
-git fetch upstream
+git fetch origin
+git pull origin master
 
 # Add Travis commit to Dockerfile
 sed -i '$ d' images/led/Dockerfile && echo 'Travis Build - GIT_SHA ${TRAVIS_COMMIT}' >> images/led/Dockerfile
