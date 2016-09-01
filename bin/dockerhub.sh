f9c23b737cf66ef765c3261fce0f317f80addeb5
bwnyasse <risbonyasse@gmail.com>
2016-09-01 10:09:00 +0200

    [TRAVIS] Add date in build version

--- bin/dockerhub.sh
+++ bin/dockerhub.sh
@@ -10,3 +10,4 @@ set -e
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
 docker tag  ${DOCKER_REPOSITORY}:${VERSION} ${DOCKER_REPOSITORY}:latest
 docker push ${DOCKER_REPOSITORY}:${VERSION}
+docker push ${DOCKER_REPOSITORY}:latest
