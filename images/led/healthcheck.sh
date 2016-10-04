fda3845decdcaa1b6b053bb5fcfd79fb29ff9599
bwnyasse <risbonyasse@gmail.com>
2016-10-04 09:45:01 +0200

    Update ES healthcheck stdout message

--- images/led/healthcheck.sh
+++ images/led/healthcheck.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+
+source logger.sh
+
+set -e
+
 while ! curl -s localhost:9200/  > /dev/null
 do
-echo "$(date) - still waiting for ES to be up and running"
+inf "Still waiting for ES to be up and running"
 sleep 1
 done
-echo "$(date) - ES is up and running"
+inf "LED - ES is up and running"
