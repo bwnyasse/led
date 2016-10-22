9f05bb1da1750fc971ae51012528e10ded42363b
bwnyasse <risbonyasse@gmail.com>
2016-10-23 00:32:51 +0200

    always remove cron file before updating

--- images/led/scripts/common.sh
+++ images/led/scripts/common.sh
@@ -52,14 +52,12 @@ log() {
 
 install_curator_cron() {
   echo "Install Curator Cron with schedule $ES_CURATOR_SCHEDULE for older than $ES_CURATOR_DAY_OLDER_THAN days"
-  if ! [ -f curator-cron ]
-  then
-    log_info "Creating cron entry to start curator at: $ES_CURATOR_SCHEDULE for older than $ES_CURATOR_DAY_OLDER_THAN days"
-    # Note: Must use tabs with indented 'here' scripts.
-    cat <<-EOF >> curator-cron
+  rm curator-cron 2>/dev/null
+  log_info "Creating cron entry to start curator at: $ES_CURATOR_SCHEDULE for older than $ES_CURATOR_DAY_OLDER_THAN days"
+  # Note: Must use tabs with indented 'here' scripts.
+  cat <<-EOF >> curator-cron
 $ES_CURATOR_SCHEDULE led_curator > /var/log/led/curator.log 2>&1
 EOF
-    crontab curator-cron
-    crond
-  fi
+  crontab curator-cron
+  crond
 }
