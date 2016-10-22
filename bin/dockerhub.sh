005e6224f54fe8e4e8cea0c1324c69d1a1714c29
bwnyasse <risbonyasse@gmail.com>
2016-10-22 22:45:48 +0200

    Fix docker tag option

--- bin/dockerhub.sh
+++ bin/dockerhub.sh
@@ -8,6 +8,7 @@ set -e
 
 if  [[ "$TRAVIS_BRANCH" == "master" ]]
 then
+  echo " Docker Hub deployment for master branch"
   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
   docker tag  ${DOCKER_REPOSITORY}:${VERSION} ${DOCKER_REPOSITORY}:latest
   docker push ${DOCKER_REPOSITORY}:${VERSION}
@@ -16,7 +17,8 @@ fi
 
 if  [[ "$TRAVIS_BRANCH" == "dev" ]]
 then
+  echo " Docker Hub deployment for dev branch"
   docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
-  docker tag  ${DOCKER_REPOSITORY}:devel
+  docker tag  ${DOCKER_REPOSITORY}:${VERSION} ${DOCKER_REPOSITORY}:devel
   docker push ${DOCKER_REPOSITORY}:devel
 fi
