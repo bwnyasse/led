03763233d24b404e3e81ab821a0eacd9a74eb70f
bwnyasse <risbonyasse@gmail.com>
2016-10-23 21:59:43 +0200

    Fix input verification in get cron value

--- images/led/scripts/get_cron_curator_value.sh
+++ images/led/scripts/get_cron_curator_value.sh
@@ -14,9 +14,9 @@ usage() {
 }
 
 NAME=$1
-[[ -n $NAME ]] && exit 1;
+[[ -n $NAME ]] || exit 1;
 POSITION=$2
-[[ -n $POSITION ]] && exit 1;
+[[ -n $POSITION ]] || exit 1;
 KEY_VALUE_LINE=$(crontab -l | sed -n "$POSITION"p)
 set -- "$KEY_VALUE_LINE"
 OIFS=$IFS; IFS="="; #Sauvegarde de l'ancien IFS et initialisation du nouveau
